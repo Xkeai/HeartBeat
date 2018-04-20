d7527cc0b1f43c9f74e15e63b3847e5b04fe4f32
Alpy <alperen.aydin@cpe.fr>
2018-04-20 16:17:55 +0200

    Switched from ggplot to matplotlib

--- src/performace_visualisation.py
+++ src/performace_visualisation.py
@@ -1,14 +1,23 @@
-from ggplot import *
-
+import matplotlib.pyplot as plt
 import pandas as pd
 
-perf_log = pd.read_csv("../logs/log_180408_1244.csv")
-perf_log_tidy = pd.melt(perf_log,
-                        id_vars=["train_step", "epoch"],
-                        value_vars=["train_loss", "valid_loss"])
+# Importing the log
+perf_log = pd.read_csv("../logs/log_180419_1358.csv")
 
 
-p = ggplot(perf_log_tidy, aes(x="train_step", y="value", colour="variable")) +\
-    geom_line()
+# Building the graph
+fig, ax = plt.subplots()
 
-print(p)
+ax.plot(perf_log["train_step"],
+        perf_log["train_loss"],
+        label="Training Loss")
+
+ax.plot(perf_log["train_step"],
+        perf_log["valid_loss"],
+        label="Validation Loss")
+
+plt.legend()
+ax.grid()
+ax.set(xlabel="Time steps", ylabel="Loss")
+
+plt.show()
